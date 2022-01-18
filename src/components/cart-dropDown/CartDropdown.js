6357a9f606182cad5f3762811fb0d7b0d2645123
nugi-developers <68037498+nugi-developers@users.noreply.github.com>
2022-01-18 12:25:09 +0100

    created cart-item component and connected the cart dropdown with cartItems reducer

--- src/components/cart-dropDown/CartDropdown.js
+++ src/components/cart-dropDown/CartDropdown.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./CartDropdown.scss";
 import CustomButton from "../customButton/CustomButton";
+import { useSelector } from "react-redux";
+import CartItem from "../cart-item component/CartItem";
 
 function CartDropdown() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'> </div>
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
